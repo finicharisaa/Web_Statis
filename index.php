695ba6243782feda389fc9eecc8d916b10b6bd26
Herdiansyah-Ajita <60681306+Herdiansyah-Ajita@users.noreply.github.com>
2020-02-21 13:00:12 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nascence</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="topnav">
+    <img src="nascence-logo.png" alt="illust" class="site-logo">
+    <ul>
+        <li><a class="active" href="#home">Home</a>
+        <li><a href="about.php">About</a>
+        <li><a href="contact.php">Contact</a>
+    </ul>
+    </div>
+
+    <div class="title-site" >
+        <span>Engage Your Customer with A Faster Way without Obstacles.</span>
+    </div>
+
+    <div class="description">
+		<span>Our flexible and powerful systems are designed to meet any scale of business. Explore our products and consult with our professional team to find out what you really need.</span>
+	</div>
+    
+    <div>
+    <a href="#explore-now" class="button">EXPLORE NOW</a>
+    </div>
+
+    <img src="Group_2324.png" alt="illust" class="img">
+
+    <div class="title-collab" id="explore-now">
+        <span>Our Collaboration</span>
+    </div>
+
+    <img src="monday-dot-com-logo-partner.png" alt="illust" class="collab-img">
+    <img src="sap-logo-partner.png" alt="illust" class="collab-img2">
+    <img src="freshworks-logo-partner.png" alt="illust" class="collab-img3">
+
+    <div class="title-features">
+        <span>Complete. Sophisticated. Customizable.</span>
+    </div>
+    <div class="features" >
+        <img src="activity.svg" alt="illust" class="features1">
+        <p>The #1 HRIS software in Indonesia to assist you in managing your HR and improving you employee experience.</p>
+    </div>
+
+    <div class="features2">
+        <img src="airplay.svg" alt="illust" class="features1">
+        <p>Support your customers & your employee, improve sales, organize your task easier with our Cloud solutions.</p>
+    </div>
+
+    <div class="features3">
+        <img src="bell.svg" alt="illust" class="features1">
+        <p>We know you need more so we provide ad-hoc solutions to customize mobile & web app adjusted to your system.</p>
+    </div>
+
+    <div class="cta">
+        <img src="cta-background.png" alt="illust" class="cta-bg">
+        <h3 class="title-cta">More about us?</h3>
+        <p class="desc-cta">Whether it's in regards to feedback, partnerships, careers, or anything else, we’re here to listen. Don’t be afraid to reach out!</p>
+        <a href="contact.php" class="button3">Free Consultation</a>
+    </div>
+
+    <div class="blank2">
+    
+    </div>
+
+</body>
+</html>
